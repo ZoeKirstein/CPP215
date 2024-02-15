0edbceaa83a85db877a1870de27c3ea8e19c3a3e
ZoeKirstein <150391867+ZoeKirstein@users.noreply.github.com>
2024-02-15 13:48:24 -0600

    Add files via upload

---
+++ 215.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+enum Color { //*numerable type// 
+    red,
+    green,
+    blue
+};
+enum Access {
+    Administrator,
+    User,
+    NoUser
+}
+
+int main(){
+    string code = "Access";
+    Color colorThing = red;    
+}
